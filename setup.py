c6cfa2f1c3f58900565fe8c39e06c77b58718be8
Tife Kusimo <49872130+ChildishClassifier@users.noreply.github.com>
2025-03-23 18:05:37 +0000

    Quick Chang

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ufc_scraper",
+    name="ufc_scraper_fighter_details",
     version="1.0.0",
     description="A Python package to scrape UFC events and fighter data",
     author="Tife Kusimo",
-    author_email="tife.kusimo@hotmail.com",
+    author_email="kushykernel@yahoo.com",
     url="https://github.com/ChildishClassifier/UFC",  # Repository URL
     packages=find_packages(),
     install_requires=[
